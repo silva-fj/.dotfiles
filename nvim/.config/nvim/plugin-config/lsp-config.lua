bc37b77bb5abfb1cccd61713d8f9a6eb4242760a
Francisco Silva <franjs.francisco@gmail.com>
2021-03-15 15:45:19 +0100

    commenting out cursor highlight in LSP config

--- nvim/.config/nvim/plugin-config/lsp-config.lua
+++ nvim/.config/nvim/plugin-config/lsp-config.lua
@@ -2,6 +2,7 @@ local nvim_lsp = require('lspconfig')
 
 local on_attach = function(client, bufnr)
   print("LSP started.");
+
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
@@ -17,18 +18,18 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
+  -- if client.resolved_capabilities.document_highlight then
+    -- vim.api.nvim_exec([[
+      -- hi LspReferenceRead cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
+      -- hi LspReferenceText cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
+      -- hi LspReferenceWrite cterm=bold ctermbg=None guibg=DarkGray guifg=LightYellow
+      -- augroup lsp_document_highlight
+        -- autocmd! * <buffer>
+        -- autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        -- autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      -- augroup END
+    -- ]], false)
+  -- end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
