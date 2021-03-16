f5e8b4153aa7f6799661caffe9809cb167bbd938
Francisco Silva <franjs.francisco@gmail.com>
2021-03-16 19:58:55 +0100

    adding list of diagnostics

--- nvim/.config/nvim/plugin-config/lsp-config.lua
+++ nvim/.config/nvim/plugin-config/lsp-config.lua
@@ -35,6 +35,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>a', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
   -- Set autocommands conditional on server_capabilities
   -- if client.resolved_capabilities.document_highlight then
