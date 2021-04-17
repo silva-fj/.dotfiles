014c0a3dd499ceb45f39526f1209dc2fd4300cec
Francisco Silva <franjs.francisco@gmail.com>
2021-04-17 15:32:22 +0200

    moving from telescope's fzy_native extension to fzf, removing media_files extension config

--- nvim/.config/nvim/lua/plugin/telescope.nvim.lua
+++ nvim/.config/nvim/lua/plugin/telescope.nvim.lua
@@ -1,17 +1,8 @@
-require('telescope').load_extension('fzy_native')
--- require('telescope').load_extension('media_files')
-
 require('telescope').setup {
-    extensions = {
-        fzy_native = {override_generic_sorter = false, override_file_sorter = true}
-        -- media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        -- filetypes = {"png", "webp", "jpg", "jpeg"},
-        -- find_cmd = "rg" -- find command (defaults to `fd`)
-        -- }
-    }
+    extensions = {fzf = {override_generic_sorter = false, override_file_sorter = true, case_mode = "smart_case"}}
 }
+
+require('telescope').load_extension('fzf')
 
 vim.api.nvim_set_keymap("n", "<Leader>bc", "<cmd>lua require('telescope.builtin').git_commits()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true})
