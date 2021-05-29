7ae37cba928e5d1f489e64443f3b772675864e10
Francisco Silva <franjs.francisco@gmail.com>
2021-05-29 18:31:12 +0200

    updating zsh config

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -4,8 +4,6 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
