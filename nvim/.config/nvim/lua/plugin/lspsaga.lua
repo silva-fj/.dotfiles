e7756dffc3698a5269e1044e3d2689a9181863df
Francisco Silva <franjs.francisco@gmail.com>
2021-06-03 17:52:55 +0200

    remapping up/down navigation for diagnostics (lspsaga)

--- nvim/.config/nvim/lua/plugin/lspsaga.lua
+++ nvim/.config/nvim/lua/plugin/lspsaga.lua
@@ -8,9 +8,9 @@ vim.api.nvim_set_keymap("v", "<Leader>a", "<cmd>lua require('lspsaga.codeaction'
                         {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",
                         {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>",
+vim.api.nvim_set_keymap("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>",
                         {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>",
+vim.api.nvim_set_keymap("n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>",
                         {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<F2>", "<cmd>lua require('lspsaga.rename').rename()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>cd", "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>",
