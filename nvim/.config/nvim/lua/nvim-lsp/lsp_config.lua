be77a81d311dd2d06e3ac58957d225fee3f0c2f2
Francisco Silva <franjs.francisco@gmail.com>
2021-07-09 16:00:07 +0200

    configuring lsp_signature

--- nvim/.config/nvim/lua/nvim-lsp/lsp_config.lua
+++ nvim/.config/nvim/lua/nvim-lsp/lsp_config.lua
@@ -9,6 +9,7 @@ vim.fn.sign_define("LspDiagnosticsSignHint",
 
 local lspconfig = require('lspconfig')
 local lsp_status = require('nvim-lsp/lsp_status')
+local lsp_signature = require('lsp_signature')
 
 local lsp_config = {}
 
@@ -16,6 +17,7 @@ function lsp_config.on_attach(client, bufnr)
     print("LSP started.");
 
     lsp_status.on_attach(client, bufnr)
+    lsp_signature.on_attach({use_lspsaga = true})
 
     local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
