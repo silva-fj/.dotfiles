5365f08f2435f82e269313aca02fe0410c2a24d4
Francisco Silva <franjs.francisco@gmail.com>
2021-11-08 14:25:26 +0100

    cleaning up tsserver lsp config

--- nvim/.config/nvim/lua/nvim-lsp/typescript_ls.lua
+++ nvim/.config/nvim/lua/nvim-lsp/typescript_ls.lua
@@ -6,9 +6,6 @@ local on_attach_tsserver = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 require('lspconfig').tsserver.setup {
-    on_attach = on_attach_tsserver,
-    capabilities = capabilities
+    on_attach = on_attach_tsserver
 }
