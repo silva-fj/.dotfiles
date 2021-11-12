4406cfae948de5e72160101011978bf3cd3be93b
Francisco Silva <franjs.francisco@gmail.com>
2021-11-12 10:30:13 +0100

    formatting typescript_ls

--- nvim/.config/nvim/lua/nvim-lsp/typescript_ls.lua
+++ nvim/.config/nvim/lua/nvim-lsp/typescript_ls.lua
@@ -2,8 +2,7 @@ local on_attach_tsserver = function(client)
     print("LSP started. (tsserver)");
     if client.config.flags then client.config.flags.allow_incremental_sync = true end
     client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
 end
 
-require('lspconfig').tsserver.setup {
-    on_attach = on_attach_tsserver
-}
+require('lspconfig').tsserver.setup {on_attach = on_attach_tsserver}
