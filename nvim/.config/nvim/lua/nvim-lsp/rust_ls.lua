16bad6388250390020120319a0a58b984a23dd0e
Francisco Silva <franjs.francisco@gmail.com>
2021-11-26 10:16:30 +0100

    adding mapping to format rust

--- nvim/.config/nvim/lua/nvim-lsp/rust_ls.lua
+++ nvim/.config/nvim/lua/nvim-lsp/rust_ls.lua
@@ -1,9 +1,8 @@
-local nvim_lsp = require'lspconfig'
+local nvim_lsp = require 'lspconfig'
 
 local on_attach = function()
+    vim.api.nvim_set_keymap("n", "ff", "<cmd>RustFmt<CR>", {noremap = true})
     print("LSP rust_analyzer started.");
 end
 
-nvim_lsp.rust_analyzer.setup({
-    on_attach=on_attach,
-})
+nvim_lsp.rust_analyzer.setup({on_attach = on_attach})
