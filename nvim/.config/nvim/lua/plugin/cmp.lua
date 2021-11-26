fbddc0ce897072a2f18b3607ccb31959685a3825
Francisco Silva <franjs.francisco@gmail.com>
2021-11-26 12:53:22 +0100

    formatting cmp.lua

--- nvim/.config/nvim/lua/plugin/cmp.lua
+++ nvim/.config/nvim/lua/plugin/cmp.lua
@@ -1,44 +1,35 @@
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  formatting = {
-      format = function(entry, vim_item)
-        vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-        vim_item.menu = ({
-            nvim_lsp = "[LSP]",
-            buffer = "[Buffer]",
-            vsnip = "[VSNIP]",
-            nvim_lua = "[Lua]",
-            path = "[PATH]"
-        })[entry.source.name]
-        return vim_item
-      end
-  },
-  mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<CR>'] = cmp.mapping({
-        i = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({ select = false }),
-     })
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'nvim_lua' }
-  },
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end
+    },
+    formatting = {
+        format = function(entry, vim_item)
+            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+            vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                buffer = "[Buffer]",
+                vsnip = "[VSNIP]",
+                nvim_lua = "[Lua]",
+                path = "[PATH]"
+            })[entry.source.name]
+            return vim_item
+        end
+    },
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+        ['<C-p>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
+        ['<CR>'] = cmp.mapping({i = cmp.mapping.confirm({select = true}), c = cmp.mapping.confirm({select = false})})
+    },
+    sources = {{name = 'nvim_lsp'}, {name = 'vsnip'}, {name = 'buffer'}, {name = 'path'}, {name = 'nvim_lua'}}
 }
