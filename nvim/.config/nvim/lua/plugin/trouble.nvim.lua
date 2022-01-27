59387d81016b168d0a3f90acab3629fa2891a164
Francisco Silva <franjs.francisco@gmail.com>
2022-01-27 16:51:51 +0100

    updating trouble.nvim mappings

--- nvim/.config/nvim/lua/plugin/trouble.nvim.lua
+++ nvim/.config/nvim/lua/plugin/trouble.nvim.lua
@@ -1,13 +1,7 @@
 require("trouble").setup()
 
-vim.api.nvim_set_keymap("n", "<leader>ge", "<cmd>Trouble lsp_document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
+vim.api.nvim_set_keymap("n", "<leader>ge", "<cmd>Trouble document_diagnostics<cr>", {silent = true, noremap = true})
 
-vim.api.nvim_set_keymap("n", "<leader>gE", "<cmd>Trouble lsp_workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
+vim.api.nvim_set_keymap("n", "<leader>gE", "<cmd>Trouble workspace_diagnostics<cr>", {silent = true, noremap = true})
 
-vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>",
-  {silent = true, noremap = true}
-)
+vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", {silent = true, noremap = true})
