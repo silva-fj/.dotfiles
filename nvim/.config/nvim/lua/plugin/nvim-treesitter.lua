c3191d23de41a65e7e5ca42156c500ad7fbfaa96
Francisco Silva <franjs.francisco@gmail.com>
2022-02-03 14:32:54 +0100

    enabling treesitter highlight for rust

--- nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugin/nvim-treesitter.lua
@@ -1,9 +1,8 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = {"c", "rust"} -- list of language that will be disabled
-    },
-    indent = {enable = true},
-    context_commentstring = {enable = true, enable_autocmd = false}
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	highlight = {
+		enable = true,
+	},
+	indent = { enable = true },
+	context_commentstring = { enable = true, enable_autocmd = false },
+})
