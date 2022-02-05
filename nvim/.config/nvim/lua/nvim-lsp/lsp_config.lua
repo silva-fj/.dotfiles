e6b5885535729013069ea270c11356f087222248
Francisco Silva <franjs.francisco@gmail.com>
2022-02-05 17:17:03 +0100

    formatting lsp_config.lua file

--- nvim/.config/nvim/lua/nvim-lsp/lsp_config.lua
+++ nvim/.config/nvim/lua/nvim-lsp/lsp_config.lua
@@ -1,41 +1,50 @@
-vim.fn.sign_define("LspDiagnosticsSignError",
-                   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning",
-                   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation",
-                   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint",
-                   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"})
+vim.fn.sign_define(
+	"LspDiagnosticsSignError",
+	{ texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignWarning",
+	{ texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignInformation",
+	{ texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignHint",
+	{ texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint" }
+)
 
-local lspconfig = require('lspconfig')
-local lsp_status = require('nvim-lsp/lsp_status')
+local lspconfig = require("lspconfig")
+local lsp_status = require("nvim-lsp/lsp_status")
 -- local lsp_signature = require('lsp_signature')
 
 local lsp_config = {}
 
 function lsp_config.on_attach(client, bufnr)
-    print("LSP started.");
+	print("LSP started.")
 
-    lsp_status.on_attach(client, bufnr)
-    -- lsp_signature.on_attach()
+	lsp_status.on_attach(client, bufnr)
+	-- lsp_signature.on_attach()
 
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>d', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
+	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "<space>d", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
+	-- Set autocommands conditional on server_capabilities
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
       hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
       hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
@@ -44,22 +53,25 @@ function lsp_config.on_attach(client, bufnr)
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
-
-    end
+    ]],
+			false
+		)
+	end
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.get_capabilities())
-local cmpCapabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-capabilities = vim.tbl_extend('keep', capabilities or {}, cmpCapabilities)
+capabilities = vim.tbl_extend("keep", capabilities or {}, lsp_status.get_capabilities())
+local cmpCapabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = vim.tbl_extend("keep", capabilities or {}, cmpCapabilities)
 
 lsp_config.capabilities = capabilities
 
 -- Use a loop to conveniently both setup defined servers and map buffer local keybindings when the language server attaches
-local servers = {"cssls", "intelephense", "vimls", "yamlls", "html", "eslint", "graphql"}
+local servers = { "cssls", "intelephense", "vimls", "yamlls", "html", "eslint", "graphql" }
 
-for _, lsp in ipairs(servers) do lspconfig[lsp].setup {on_attach = lsp_config.on_attach, capabilities = capabilities} end
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({ on_attach = lsp_config.on_attach, capabilities = capabilities })
+end
 
 return lsp_config
