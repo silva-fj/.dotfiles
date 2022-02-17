a83e6abc9e0abe88a4187e7300e39840be870b5e
Francisco Silva <franjs.francisco@gmail.com>
2022-02-17 20:00:00 +0100

    disable autocomplete for search buffer

--- nvim/.config/nvim/lua/plugin/cmp.lua
+++ nvim/.config/nvim/lua/plugin/cmp.lua
@@ -1,48 +1,56 @@
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-        end
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-            vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                buffer = "[Buffer]",
-                vsnip = "[VSNIP]",
-                nvim_lua = "[Lua]",
-                path = "[PATH]"
-            })[entry.source.name]
-            return vim_item
-        end
-    },
-    mapping = {
-        ['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
-        ['<C-p>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        -- ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end,
-        ['<CR>'] = cmp.mapping({i = cmp.mapping.confirm({select = true}), c = cmp.mapping.confirm({select = false})})
-    },
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'}, {name = 'vsnip'}, {name = 'buffer'}, {name = 'path'}, {name = 'nvim_lua'}
-    })
-}
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				buffer = "[Buffer]",
+				vsnip = "[VSNIP]",
+				nvim_lua = "[Lua]",
+				path = "[PATH]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	mapping = {
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
+		["<CR>"] = cmp.mapping({
+			i = cmp.mapping.confirm({ select = true }),
+			c = cmp.mapping.confirm({
+				select = false,
+			}),
+		}),
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "nvim_lua" },
+	}),
+})
 
-cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
+-- cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
 
-cmp.setup.cmdline(':', {sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})})
+cmp.setup.cmdline(":", { sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }) })
