f678ee582a7293ea9cccde1366add49fdafa5ec9
Francisco Silva <franjs.francisco@gmail.com>
2022-05-30 09:04:36 +0200

    updating nvim-tree config

--- nvim/.config/nvim/lua/plugin/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugin/nvim-tree.lua
@@ -1,50 +1,9 @@
-vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "✓",
-		unmerged = "",
-		renamed = "➜",
-		untracked = "★",
-		deleted = "",
-		ignored = "◌",
-	},
-	folder = {
-		arrow_open = "",
-		arrow_closed = "",
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-	},
-	lsp = {
-		hint = "",
-		info = "",
-		warning = "",
-		error = "",
-	},
-}
-vim.g.nvim_tree_show_icons = {
-	git = 1,
-	folders = 1,
-	files = 1,
-	-- folder_arrows = 1, nvim_tree_indent_markers needs to be disabled
-}
-vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_auto_ignore_ft = { "startify" }
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_root_folder_modifier = ":~"
-vim.g.nvim_tree_width_allow_resize = 1
-
 vim.api.nvim_set_keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>R", ":NvimTreeRefresh<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>F", ":NvimTreeFindFile<CR>", { noremap = true })
 
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
--- default mappings
+
 local mappingsList = {
 	{ key = { "<CR>", "o", "<2-LeftMouse>", "l" }, cb = tree_cb("edit") },
 	{ key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
@@ -78,34 +37,68 @@ local mappingsList = {
 	{ key = "q", cb = tree_cb("close") },
 	{ key = "g?", cb = tree_cb("toggle_help") },
 }
+
 require("nvim-tree").setup({
-	-- disables netrw completely
 	disable_netrw = true,
-	-- hijack netrw window on startup
 	hijack_netrw = true,
-	-- open the tree when running this setup function
 	open_on_setup = false,
-	-- will not open on setup if the filetype is in this list
-	ignore_ft_on_setup = {},
-	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	ignore_ft_on_setup = { "startify" },
 	open_on_tab = false,
-	-- hijack the cursor in the tree to put it at the start of the filename
 	hijack_cursor = false,
-	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
 	update_cwd = false,
 	renderer = {
+		add_trailing = false,
+		group_empty = false,
+		highlight_git = false,
+		highlight_opened_files = "none",
+		root_folder_modifier = ":~",
 		indent_markers = {
-			enable = true,
+			enable = false,
 			icons = {
 				corner = "└ ",
 				edge = "│ ",
 				none = "  ",
 			},
 		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = false,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
 	},
-	-- show lsp diagnostics in the signcolumn
 	diagnostics = {
 		enable = true,
+		show_on_dirs = true,
 		icons = {
 			hint = "",
 			info = "",
@@ -113,36 +106,20 @@ require("nvim-tree").setup({
 			error = "",
 		},
 	},
-	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
 	update_focused_file = {
-		-- enables the feature
 		enable = true,
-		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-		-- only relevant when `update_focused_file.enable` is true
 		update_cwd = false,
-		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
 		ignore_list = {},
 	},
-	-- configuration options for the system open command (`s` in the tree by default)
 	system_open = {
-		-- the command to run this, leaving nil should work in most cases
 		cmd = nil,
-		-- the command arguments as a list
 		args = {},
 	},
-
 	view = {
-		-- width of the window, can be either a number (columns) or a string in `%`
 		width = 40,
-		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
 		side = "left",
-
 		mappings = {
-			-- custom only false will merge the list with the default mappings
-			-- if true, it will only use your list to set the mappings
 			custom_only = false,
-			-- list of mappings to set on the tree manually
 			list = mappingsList,
 		},
 	},
