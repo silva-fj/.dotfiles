1ad72a4b225e6d6d6b18711a8edac233372b3f13
Francisco Silva <franjs.francisco@gmail.com>
2022-10-31 09:31:21 +0100

    updating formatting command

--- nvim/.config/nvim/lua/nvim-lsp/rust_ls.lua
+++ nvim/.config/nvim/lua/nvim-lsp/rust_ls.lua
@@ -2,4 +2,4 @@ vim.g.rustfmt_autosave = 1
 
 require("rust-tools").setup({})
 
-vim.api.nvim_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
