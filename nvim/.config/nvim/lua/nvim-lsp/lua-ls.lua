6187da2f4015029cb8302f5ef428eabcba4c0165
Francisco Silva <franjs.francisco@gmail.com>
2022-11-12 10:50:16 +0100

    updating lua-language-server config

--- nvim/.config/nvim/lua/nvim-lsp/lua-ls.lua
+++ nvim/.config/nvim/lua/nvim-lsp/lua-ls.lua
@@ -1,23 +1,4 @@
--- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
-USER = vim.fn.expand("$USER")
-
-local sumneko_root_path = ""
-local sumneko_binary = ""
-
-if vim.fn.has("mac") == 1 then
-	sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
-	sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
-elseif vim.fn.has("unix") == 1 then
-	sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lua-language-server"
-	sumneko_binary = "/home/" .. USER .. "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
-else
-	print("Unsupported system for sumneko")
-end
-
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
+-- https://github.com/sumneko/lua-language-server/wiki/Getting-Started
 require("lspconfig").sumneko_lua.setup({
 	on_attach = function(client)
 		require("illuminate").on_attach(client)
@@ -29,14 +10,11 @@ require("lspconfig").sumneko_lua.setup({
 		)
 		print("LSP lua started.")
 	end,
-	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
 	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
@@ -44,16 +22,14 @@ require("lspconfig").sumneko_lua.setup({
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-					["${3rd}/lovr/library"] = true,
-				},
+				library = vim.api.nvim_get_runtime_file("", true),
 				maxPreload = 4000,
 				preloadFileSize = 4000,
 				checkThirdParty = false,
 			},
-			telemetry = { enable = false },
+			telemetry = {
+				enable = false,
+			},
 		},
 	},
 })
